9d04a2ad32e3429adfd9665c0cdfb2b97cfc7fdd
Ziv <zivpeer@gmail.com>
2020-02-05 19:58:28 +0200

    added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
-    return gulp.src('scss/styles.scss')
+    return gulp.src('scss/*')
+        .pipe(concat("styles.css"))
         .pipe(sass())
-        .pipe(gulp.dest('css/'))
-})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'));
+});
 
-// gulp.task('build', gulp.series(
-//     clean
-// ));
+gulp.task('gulpJs', function () {
+    return gulp.src('js_modules/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('js/'));
+});
+
+gulp.task('default', gulp.series('sass', 'gulpJs'));
+
+gulp.task('watch', function () {
+    gulp.watch('scss/*', gulp.series('sass'));
+    gulp.watch('js_modules/*.js', gulp.series('gulpJs'));
+});
