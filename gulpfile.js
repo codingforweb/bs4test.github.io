e6dfafd9649d7230efb6f4a24a046fd3af71af60
Ziv <zivpeer@gmail.com>
2020-02-06 14:33:31 +0200

    folders refactoring v1

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,22 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
-    return gulp.src('scss/*')
-        .pipe(concat("styles.css"))
+    return gulp.src('src/scss/*')
         .pipe(sass())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css/'));
+        .pipe(concat("styles.css"))
+        .pipe(gulp.dest('prod/css/'));
 });
 
 gulp.task('gulpJs', function () {
-    return gulp.src('js_modules/*.js')
+    return gulp.src('src/js_modules/*.js')
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('prod/js/'));
 });
 
 gulp.task('default', gulp.series('sass', 'gulpJs'));
 
 gulp.task('watch', function () {
-    gulp.watch('scss/*', gulp.series('sass'));
-    gulp.watch('js_modules/*.js', gulp.series('gulpJs'));
+    gulp.watch('src/scss/*', gulp.series('sass'));
+    gulp.watch('src/js_modules/*.js', gulp.series('gulpJs'));
 });
